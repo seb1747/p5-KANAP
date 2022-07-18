a711e40e4d88bded2175a8bfa59cd09b277ac9de
sebastien binet <sebastienbinet47@gmail.com>
2022-07-18 10:47:00 +0200

    ebauche product

---
+++ front/js/cart.js
@@ -0,0 +1,259 @@
+// récpurer le tableau dans le localStorage
+let registerItem = JSON.parse(localStorage.getItem("product"));
+
+// fonction qui indique la quantité et calcul le total des produits
+ function totalQuantity(params) {
+    let totalQuantity = document.getElementById('totalQuantity');
+    let quantitiesProduct = document.querySelectorAll('.itemQuantity');
+    let totalQuantities = 0;
+    quantitiesProduct.forEach(qte =>{
+    totalQuantities += Number(qte.value);
+    })
+    return totalQuantity.innerHTML = totalQuantities;
+}
+
+// fonction qui calcul le prix des produits par quantité et le prix total
+function totalPrice(params) {
+    let totalPrice = document.getElementById("totalPrice")
+    let priceItem = document.querySelectorAll(".priceItem")
+    total = 0;
+    priceItem.forEach(price => {
+        total += Number(price.textContent);
+        console.log(typeof price.textContent);
+    })
+    return totalPrice.innerHTML = total;
+}
+
+///////////////////PARTIE PANIER
+
+
+// si un produit est dans le local storage
+if (registerItem) {
+    
+    // construction du DOM avec une boucle
+    registerItem.forEach(eltItem => {
+        
+        let cartArticles = document.getElementById("cart__items")
+
+        let article = document.createElement("article");
+            article.classList.add("cart__item");
+            article.setAttribute("data-id", eltItem.id)
+            cartArticles.appendChild(article);
+        
+        let cartImg = document.createElement("div");
+            cartImg.classList.add("cart__item__img");
+            article.appendChild(cartImg);
+        
+        let imgItem = document.createElement("img");
+            cartImg.appendChild(imgItem);
+            imgItem.src = eltItem.image;
+            imgItem.alt = eltItem.imageAlt;       
+            
+        let cartContent = document.createElement("div");
+            cartContent.classList.add("cart__item__content");
+            article.appendChild(cartContent);
+        
+        let contenTitlePrice = document.createElement("div");
+            contenTitlePrice.classList.add("cart__item__content__titlePrice");
+            cartContent.appendChild(contenTitlePrice);
+        let nameItem = document.createElement("h2")
+            nameItem.innerHTML = eltItem.name;
+        let priceItem = document.createElement("p");
+            priceItem.classList.add("priceItem");
+            // +" "+"€";
+            contenTitlePrice.appendChild(nameItem);
+            contenTitlePrice.appendChild(priceItem);
+            
+        let itemSettings = document.createElement("div");
+            itemSettings.classList.add("cart__item__content__settings");
+            cartContent.appendChild(itemSettings);
+            
+        let settingsQuantity = document.createElement("div");
+            settingsQuantity.classList.add("cart__item__content__settings__quantity");
+            itemSettings.appendChild(settingsQuantity);
+        let quantityTitle = document.createElement("p");
+            quantityTitle.innerHTML ="Quantité:" ;
+            settingsQuantity.appendChild(quantityTitle);
+            
+        let inputQuantity = document.createElement("input");
+            inputQuantity.classList.add("itemQuantity");
+            inputQuantity.type="number";
+            inputQuantity.name="itemQuantity";
+            inputQuantity.min="1";
+            inputQuantity.max="100";
+            inputQuantity.value = eltItem.quantities;
+            settingsQuantity.appendChild(inputQuantity);
+            
+        let settingsDelete = document.createElement("div");
+            settingsDelete.classList.add("cart__item__content__settings__delete");
+            cartContent.appendChild(settingsDelete);
+
+        let deleteItem = document.createElement("p")
+            deleteItem.classList.add("deleteItem");
+            deleteItem.innerHTML="Supprimer";
+            settingsDelete.appendChild(deleteItem);
+            
+        // A l'ouverture du panier, calcul des tarifs et des quantités déjà présents
+        priceItem.innerHTML=inputQuantity.value * eltItem.price;
+        totalPrice()
+        totalQuantity()
+            
+        // changement des quantités avec la maj des tarifs, des quantités et du local storage
+        inputQuantity.addEventListener("change",(e)=>{
+            e.preventDefault();
+            priceItem.innerHTML=inputQuantity.value * eltItem.price;
+            totalQuantity();
+            totalPrice();
+            if (localStorage.getItem('product')) {
+                let objIndex = registerItem.findIndex((item=> item.id === eltItem.id && item.color === eltItem.color))
+                if (objIndex !== -1) {
+                    registerItem[objIndex].quantities = inputQuantity.value;
+                }
+            }
+            localStorage.setItem("product",JSON.stringify(registerItem));
+        })
+
+        // suppression des articles avec maj du local storage
+        deleteItem.addEventListener('click',(e) =>{
+            e.preventDefault();
+            if (eltItem.id === eltItem.id) {
+                let objIndex = registerItem.indexOf();
+                // demande de confirmation de la suppression de l'article
+                deleteConfirm = confirm("Voulez-vous vraiment supprimer l'article?")
+                if (deleteConfirm == true) {
+                   cartArticles.removeChild(article);
+                   registerItem.splice(objIndex, 1)
+                }
+            }
+            totalPrice();
+            totalQuantity();
+            localStorage.setItem('product',JSON.stringify(registerItem));
+            // si le localStorage n'as plus d'article faire un clear et proposer de retourner à la page d'acceuil
+            if (registerItem.length == 0 ) {
+                localStorage.clear();
+                returnPageIndex = window.confirm("Panier vide, voulez-vous retourner à la page d'accueil")
+                if (returnPageIndex == true) {
+                    location.href ="index.html"
+                }
+            }
+        });
+    });
+}
+
+///////////////////PARTIE FORMULAIRE
+
+// je déclrare des variables pour récupérer les id, pour créer des regex et les messages d'erreurs
+
+let firstName = document.getElementById('firstName');
+let regexName = /^[a-z ,.'-]+$/i;
+let errorFirstName = document.getElementById('firstNameErrorMsg');
+
+let lastName = document.getElementById('lastName');
+let errorLastName = document.getElementById('lastNameErrorMsg');
+
+let address = document.getElementById('address');
+let regexAddress = /^[a-zA-Z0-9\s,'-]*$/;
+let errorAddress = document.getElementById('addressErrorMsg');
+
+let city = document.getElementById('city');
+let regexCity=/^[a-zA-Z\u0080-\u024F]+(?:([\ \-\']|(\.\ ))[a-zA-Z\u0080-\u024F]+)*$/;
+let errorCity = document.getElementById('cityErrorMsg');
+
+let email = document.getElementById('email');
+let regexEmail=/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+let errorEmail = document.getElementById('emailErrorMsg');
+
+let order = document.getElementById('order');
+
+// évènements en input afin d'indiquer un message d'erreur si un mauvais caractère est utilisé
+firstName.addEventListener('input',(e)=>{
+    e.preventDefault();
+    if (regexName.test(firstName.value)==false) {
+        errorFirstName.innerHTML = "Prénom est incorrect";
+    }else{
+        errorFirstName.innerHTML = "";
+    }
+});
+
+lastName.addEventListener('input',(e)=>{
+    e.preventDefault();
+    if (regexName.test(lastName.value)==false) {
+        errorLastName.innerHTML = "Nom est incorrect";
+    }else{
+        errorLastName.innerHTML = "";
+    }
+});
+
+address.addEventListener('input',(e)=>{
+    e.preventDefault();
+    if (regexAddress.test(address.value)==false) {
+        errorAddress.innerHTML = "Addresse est incorrect";
+    }else{
+        errorAddress.innerHTML = "";
+    }
+});
+
+city.addEventListener('input',(e)=>{
+    e.preventDefault();
+    if (regexCity.test(city.value)==false) {
+        errorCity.innerHTML = "Ville incorrect";
+    }else{
+        errorCity.innerHTML = "";
+    }
+});
+
+email.addEventListener('input',(e)=>{
+    e.preventDefault();
+    if (regexEmail.test(email.value)==false) {
+        errorEmail.innerHTML = "Email incorrect";
+    }else{
+        errorEmail.innerHTML = "";
+    }
+});
+
+// évènement au clic du bouton commender
+order.addEventListener('click',(e)=>{
+    e.preventDefault();
+    // création d'un tableau afin de récuperer les données de l'utilisateur
+    let contact = {
+        firstName : firstName.value,
+        lastName : lastName.value,
+        address : address.value,
+        city : city.value,
+        email : email.value,
+    }
+    // si des données de l'utilisateur sont manquantes, un message d'erreur apparait
+    if (firstName.value === ""|| lastName.value === ""|| address.value === "" || city.value === "" || email.value === "") {
+        window.confirm("champs manquant !!")
+        // la page ne se réactualise pas 
+        window.onbeforeunload;
+    // sinon je créé un tableau et j'envoi les données     
+    }else{
+        
+        let products = [];
+
+        // boucle du tableau du localStorage afin de récupérer les id et les intégrer dans mon tableau products 
+        registerItem.forEach(order => {
+        products.push(order.id)
+        });
+
+        let pageOrder = { contact , products};
+
+        // je fais appel à l'api order pour envoyer mes tableaux
+        fetch(('http://localhost:3000/api/products/order'),{
+            method: "POST",
+            headers :{'Accept':'application/json','Content-type':'application/json'
+            },
+            body : JSON.stringify(pageOrder)
+        })
+        .then(res =>{
+            return res.json();
+        })
+        .then((data)=>{
+        window.location.href =`confirmation.html?orderId=${data.orderId}`;
+        })
+        .catch((error)=>{
+            alert(error);
+        })
+    }
+});
--- front/js/product.js
+++ front/js/product.js
@@ -2,27 +2,87 @@ let idRecuperation = new URL(window.location.href).searchParams.get('id');
 //recupération des paramètres ID depuis l'adresse URL
 console.log(idRecuperation);
 
-fetch('http://localhost:3000/api/products/' + idRecuperation)
-  .then((response) => {
-    if (response.ok) return response.json();
-    else return res.status(500).json({ error: 'Une erreur est survenue.' });
-  })
-  .then((product) => {
-    console.log(product);
-    //const canape = product.colors;
-    document.querySelector(
-      '#description'
-    ).innerHTML += `${product.description}`;
-    document.querySelector('#title').innerHTML += `${product.name}`;
-    document.querySelector('#price').innerHTML += `${product.price}`;
-    document.querySelector(
-      '.item__img'
-    ).innerHTML += `<img src='${product.imageUrl}' alt='${product.altTxt}' />`;
+fetch('http://localhost:3000/api/products/' + idRecuperation) // appel des elements de l'api
+  .then((res) => {
+    
 
-    for (let color of product) {
-      const optionColor = document.createElement('option');
-      document.querySelector('#colors').appendChild(optionColor);
-      optionColor.value += color;
-      optionColor.innerHtml += color;
-    }
-  });
+    const response = res.json();
+    //récupération du produit par son id 
+    response.then(product => {
+      console.log(product);
+      
+      // création et intégration des élémentsq du DOM
+
+      let addToCart = document.getElementById('addToCart');
+      let colors = document.getElementById('colors');
+      let img = document.createElement('img');
+      let quantity = document.querySelector('#quantity');
+      let price = document.getElementById('price').innerHTML = product.price;
+      let name = document.getElementById('title').innerHTML = product.name;
+      document.querySelector(".item__img").appendChild(img);
+      document.querySelector("#colors").insertAdjacentHTML('beforeend',product.colors.map((color) =>`<option id= "valueColor" value="${color}">${color}</option>`));
+      document.getElementById("description").innerHTML = product.description;
+      img.src = product.imageUrl;
+      img.alt = product.altTxt;
+      let image = img.src;
+      let imageAlt = img.alt;
+
+      class objBasket {
+        constructor(){
+          this.id = idRecuperation;
+          this.name = product.name;
+          this.description = product.description;
+          this.imageUrl = product.imageUrl;
+          this.altText = product.altText;
+    
+        }
+      }
+      console.log(product.name);
+
+      // ecoute du bouton au click 
+      addToCart.addEventListener("click",(e) =>{
+        e.preventDefault();
+        let color = colors.value;
+        let quantities = Number(quantity.value);
+
+        // création des données du du tableau pour le local storage 
+
+        let productInfo = new objBasket ;
+          
+      
+        
+        // création du tableau vide pour récupérer les données 
+        let registerItem = [];
+          console.log(registerItem);
+        // condition si le local storage contient un produit 
+        if (localStorage.getItem("product")) {
+          // si des données existe, elles sont transférer dans le tableau
+          registerItem  = JSON.parse(localStorage.getItem("product"));
+
+          // variable de vérification couleur quantité 
+          let compareItem = registerItem.findIndex((item=> item.id === productInfo.id &&  item.color === productInfo.color ));
+
+          // si même couleur on incrémente la quantité 
+
+          if (compareItem !== -1) {
+            registerItem[objBasket].quantities += quantities;
+          }
+          // si le produit n'existe pas dans le tableau je le rajoute 
+          else if (compareItem === -1){
+            registerItem.push(productInfo)
+          }
+
+          // envoi des produits dans le localStorage et conversion des chaine de caractère
+          localStorage.setItem("product", JSON.stringify(registerItem));
+        }else{
+          registerItem.push(productInfo);
+          localStorage.setItem("product", JSON.stringify(registerItem))
+        }
+        
+        console.log(productInfo);
+        console.log(registerItem);
+      });
+      });
+    });
+
+    
